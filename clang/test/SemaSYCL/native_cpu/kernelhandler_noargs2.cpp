21925b4607785b1cad25a8587df64e7e90963806
pietro.ghiglio <pietro.ghiglio@codeplay.com>
2023-04-07 12:05:07 +0100

    update kernelhandler_noargs2 test

--- clang/test/SemaSYCL/native_cpu/kernelhandler_noargs2.cpp
+++ clang/test/SemaSYCL/native_cpu/kernelhandler_noargs2.cpp
@@ -1,5 +1,5 @@
 // cmdline that used to fail in kernel handler emission
-// RUN: %clangxx -fsycl-device-only  -D __SYCL_NATIVE_CPU__ -mllvm -sycl-native-cpu -Xclang -fsycl-native-cpu-header=%t-hc.h -sycl-std=2020 -mllvm -sycl-opt -S -emit-llvm  -o - %s
+// RUN: %clangxx -fsycl-device-only  -fsycl-native-cpu -Xclang -fsycl-native-cpu-header=%t-hc.h -sycl-std=2020 -mllvm -sycl-opt -S -emit-llvm  -o - %s
 // RUN: FileCheck -input-file=%t-hc.h %s --check-prefix=CHECK-HC
 // RUN: %clangxx -fsycl -c -x c++ %t-hc.h
 #include "sycl.hpp"
@@ -16,6 +16,6 @@ int main() {
 //CHECK-HC: #pragma once
 //CHECK-HC-NEXT: #include <sycl/detail/native_cpu.hpp>
 //CHECK-HC:extern "C" void _ZZ4mainE10TestKernel();
-//CHECK-HC:inline static void _ZZ4mainE10TestKernelsubhandler(const std::vector<sycl::detail::NativeCPUArgDesc>& MArgs, _hc_state *state) {
+//CHECK-HC:inline static void _ZZ4mainE10TestKernelsubhandler(const std::vector<sycl::detail::NativeCPUArgDesc>& MArgs, nativecpu_state *state) {
 //CHECK-HC-NEXT:  _ZZ4mainE10TestKernel();
 //CHECK-HC-NEXT:};
